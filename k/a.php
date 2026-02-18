10ef26f5bd3111b6b8501d3b1d2f9639435e33ad
ruechakorn <66010914127@msu.ac.th>
2026-02-18 16:42:16 +0700

    a

---
+++ k/a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ฤชากร นัคราภิบาล 66010914127</title>
+</head>
+<body>
+    <h1>ฤชากร นัคราภิบาล 66010914127</h1>
+    <button type="button" name="Button" onclick="window.location='teacher.jpg';">GO TO reg.msu</button>
+</body>
+</html>
