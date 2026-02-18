64eecf6d50f345aaf7a2c33c6ea77be365b5ce79
ruechakorn <66010914127@msu.ac.th>
2026-02-18 16:52:27 +0700

    a

--- k/a.php
+++ k/a.php
@@ -7,8 +7,8 @@
 </head>
 <body>
     <h1>ฤชากร นัคราภิบาล 66010914127</h1>
-    <button type="button" style="background: green; color: green;" name="Button" onclick="window.location='teacher.jpg';">GO TO reg.msu</button>
-    <button type="button" style="background: green; color: yellow;" colour="orange" name="Button" onclick="window.location='1.jpg';">GO TO ME</button>
+    <button type="button" style="background: green; color: yellow;" name="Button" onclick="window.location='teacher.jpg';">GO TO reg.msu</button>
+    <button type="button" style="background: yellow; color: green;" colour="orange" name="Button" onclick="window.location='1.jpg';">GO TO ME</button>
 
 </body>
 </html>
