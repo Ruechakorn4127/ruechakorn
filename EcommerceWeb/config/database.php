d2897cf3b3d353c03aedc4a735d36201b403f6bb
ruechakorn <66010914127@msu.ac.th>
2026-02-18 11:21:34 +0700

    1

--- EcommerceWeb/config/database.php
+++ EcommerceWeb/config/database.php
@@ -3,7 +3,7 @@ class Database {
     private $host = "localhost";
     private $db_name = "ecommerce_db";
     private $username = "root";
-    private $password = "";
+    private $password = "666";
     public $conn;
 
     public function getConnection() {
