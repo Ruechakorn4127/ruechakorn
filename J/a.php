2bb699c2f62eeb0b10dec6c29f0cd26af94d4a31
ruechakorn <66010914127@msu.ac.th>
2026-02-18 13:28:36 +0700

    a

---
+++ J/a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>66010914127 ฤชากร นัคราภิบาล</title>
+</head>
+<body>
+    <h1>66010914127 ฤชากร นัคราภิบาล</h1>
+    <img src = "1.jpg"class="profile-img" height="500px">
+</body>
+</html>
